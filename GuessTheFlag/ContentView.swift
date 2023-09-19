8046839ce456649bafce5273c518129bf91a2d53
IvanTilev <iivantilev@gmail.com>
2023-09-19 22:06:12 +0300

    Adding the tapped flag logic and the next question logic and showing the if the user guess or not and overall score in the game

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "US" ]
-    var correnctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "US" ].shuffled()
+    @State private var correnctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -24,7 +28,7 @@ struct ContentView: View {
                 }
                 ForEach(0..<3) { number in
                     Button {
-                        // Flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -32,6 +36,29 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Contimue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+    }
+    
+    // Flag tapped function
+    func flagTapped(_ number: Int) {
+        if number == correnctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    // Ask question method
+    func askQuestion() {
+        countries.shuffle()
+        correnctAnswer = Int.random(in: 0...2)
     }
 }
 
